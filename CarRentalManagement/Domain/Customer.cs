8fc577c20e65de2893bb95bde0be8556de4be1d1
WangHsienZheng <2300690A@student.tp.edu.sg>
2024-11-26 21:04:58 +0800

    Added database and seed data

---
+++ CarRentalManagement/Domain/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRentalManagement.Domain
+{
+    public class Customer : BaseDomainModel
+    {
+        string? DrivingLicense {  get; set; }
+        string? Address { get; set; }
+        string? ContactNumber { get; set; }
+        string? EmailAddress { get; set; }
+
+    }
+}
